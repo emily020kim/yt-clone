37638f5cbf3b1ff4783576c790b62d9ac32bb8f7
emily020kim <emily020kim@gmail.com>
2023-11-28 21:19:02 -0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { PageHeader } from './layouts/PageHeader';
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='max-h-screen flex flex-col'>
+      <PageHeader />
+    </div>
+  )
+}
---
+++ src/layouts/PageHeader.tsx
@@ -0,0 +1,50 @@
+import { Menu, Bell, User, Upload, Mic, Search } from 'lucide-react';
+import logo from '../assets/watchtube.png';
+import { Button } from '../components/Button';
+
+export function PageHeader() {
+    return (
+        <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
+            <div className="flex gap-4 items-center flex-shrink-0">
+                <Button variant='ghost' size='icon'>
+                    <Menu />
+                </Button>
+                <a href='/'>
+                    <img src={logo} className='h-6' />
+                </a>
+            </div>
+            {/* issue with hidden */}
+            <form className='hidden md:flex gap-4 flex-grow justify-center'>
+                <div className='flex flex-grow max-w-[600px]'>
+                    <input 
+                        type='search' 
+                        placeholder='Search' 
+                        className='rounded-l-full border border-secondary-border shadow-inner shadow-secondary py-1 px-1 text-lg w-full focus:border-blue-500 outline-none' />
+                    <Button className='py-2 px-4 rounded-r-full border-secondary-border border border-l-0 flex-shrink-0'>
+                        <Search />
+                    </Button>
+                </div>
+                <Button type='button' size='icon' className='flex-shrink-0'>
+                    <Mic />
+                </Button>
+            </form>
+            <div className='flex flex-shrink-0 md:gap-2'>
+                <Button size='icon' variant='ghost' className='md:hidden'>
+                    <Search />
+                </Button>
+                <Button size='icon' variant='ghost' className='md:hidden'>
+                    <Mic />
+                </Button>
+                <Button size='icon' variant='ghost'>
+                    <Upload />
+                </Button>
+                <Button size='icon' variant='ghost'>
+                    <Bell />
+                </Button>
+                <Button size='icon' variant='ghost'>
+                    <User />
+                </Button>
+            </div>
+        </div>
+    )
+}
